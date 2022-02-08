d390971ef201f3df1f0920d18930ea12978ea7ae
barondiego <diegoivanbaron@gmail.com>
2022-02-08 08:25:35 -0500

    "8/2/22"

--- inv_tanques/settings.py
+++ inv_tanques/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'inv_tanques.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inv_tanques_db',
-        'USER': 'postgres',
-        'PASSWORD': 'vale1234',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
